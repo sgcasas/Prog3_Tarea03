bf83c79bd10cbc61e4948a3ba9f27a6f98686741
sgcasas <sebastian.casas.m@utec.edu.pe>
2026-04-20 15:49:29 -0500

    5. completa

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ public:
     void status() {
         cout<<"Nombre: "<<nombre<<endl;
         cout<<"Nivel: "<<nivel<<endl;
-        cout<<"Posicion: "<<posicion[0]<<" "<<posicion[1];
+        cout<<"Posicion: "<<posicion[0]<<" "<<posicion[1]<<endl;
         cout<<"Vida: "<<vida<<endl;
         cout<<"Recursos: "<<recursos<<endl;
     }
@@ -34,12 +34,43 @@ public:
 
 using Command = function<void(const list<string>&)>;
 
-class CommanCenter {
+void commandDamage(Entity& entity, const list<string>& args) {
+    if (args.empty()) {
+        cout<<"Error, el comando damage requiere un argumento"<<endl;
+        return;
+    }
+    try {
+        entity.damage(stoi(args.front()));
+    } catch (invalid_argument&) {
+        cout<<"Error, el comando damage requiere un argumento numerico"<<endl;
+    }
+}
+
+class HealFunctor {
+    Entity& entity;
+    int ejecuciones;
+public:
+    HealFunctor(Entity& entity) : entity(entity), ejecuciones(0) {}
+    void operator() (const list<string>& args) {
+        if (args.empty()) {
+            cout<<"Error, el comando heal requiere un argumento"<<endl;
+            return;
+        }
+        try {
+            entity.heal(stoi(args.front()));
+            ejecuciones++;
+        } catch (invalid_argument&) {
+            cout<<"Error, el comando heal requiere un argumento numerico"<<endl;
+        }
+    }
+};
+
+class CommandCenter {
     map<string, Command> commands;
     list<string> history;
     Entity& entity;
 public:
-    CommanCenter(Entity& entity) : entity(entity) {}
+    CommandCenter(Entity& entity) : entity(entity) {}
     void registerCommand(const string& nombre, Command cmd) {
         commands[nombre] = cmd;
     }
@@ -48,10 +79,35 @@ public:
         if (it != commands.end()) {
             it->second(args);
             history.push_back(nombre);
+        } else {
+            cout<<"Error, el comando no existe"<<endl;
         }
     }
 };
 
 int main() {
+    Entity jugador("Jugador", 100, 10, 1);
+    CommandCenter center(jugador);
+    HealFunctor healFunctor(jugador);
+    center.registerCommand("damage", [&jugador](const list<string>& args) {
+        commandDamage(jugador, args);
+    });
+    center.registerCommand("move", [&jugador](const list<string>& args) {
+        if (args.size() < 2) {
+            cout<<"Error, el comando requiere dos argumentos"<<endl;
+            return;
+        }
+        auto p = args.begin();
+        try {
+            int x = stoi(*p);
+            p++;
+            int y = stoi(*p);
+            jugador.movexy(x, y);
+
+        } catch (invalid_argument&) {
+            cout<<"Error, el comando requiere dos argumentos numericos"<<endl;
+        }
+    });
+    center.registerCommand("heal", healFunctor);
     return 0;
 }
